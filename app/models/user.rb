584e37e9af2ff573a1ac72e2ae4817ff89d8afee
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-10 15:33:14 +0530

    model setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Access the Relationship Object.
+  has_many :followed_users,
+           foreign_key: :follower_id,
+           class_name: "Relationship",
+           dependent: :destroy
+
+  # Accesses the user through the relationship object.
+  has_many :followees, through: :followed_users, dependent: :destroy
+
+  # Access the Relationship Object.
+  has_many :following_users,
+           foreign_key: :followee_id,
+           class_name: "Relationship",
+           dependent: :destroy
+
+  # Accesses the user through the relationship object.
+  has_many :followers, through: :following_users, dependent: :destroy
 end
