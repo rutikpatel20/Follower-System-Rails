25f40cbc76f5d23c1db4e2ba9eb317112a188457
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-10 15:03:24 +0530

    generate controllers and setup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "p/:id", to: "profile#show"
+
+  post "profile/follow", to: "profile#follow"
+  delete "profile/unfollow", to: "profile#unfollow"
+  root "pages#home"
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
